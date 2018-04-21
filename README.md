0c50931bc27e73c42a4c229a7519f662f96f0afe
Neel Crasta <neeldc2@gmail.com>
2018-04-22 02:32:45 +0530

    basics2

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # SpringJBAOP
 Java Brains Spring AOP Concepts
 
-In this code, there are 2 advices.
-So changes are only in LoggingAspect class.
+In this code, pointcut is added.
+Note that the pointcut method is not executed.
+Changes are only in LoggingAspect class.
 
 Output
 Advice run. Get Method called.
-After Get Method.
+Second Advice run.
 Advice run. Get Method called.
-After Get Method.
+Second Advice run.
 Circle Name
--- src/main/java/com/aop/spring/aspect/LoggingAspect.java
+++ src/main/java/com/aop/spring/aspect/LoggingAspect.java
@@ -1,28 +1,25 @@
 package com.aop.spring.aspect;
 
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	// Bean of this class has to be created in spring.xml
-	// While creating a bean, bean id isn't required because 
-	// this bean is used only for initialisation and 
-	// is not being referenced anywhere else in the application.
-
-	// each method of this class is an "advice".
-	// advice is a standard name used in AOP.
-	// here the pointcut is execution(public getName())
-	@Before("execution(public * get*(..))")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get Method called.");
 	}
 	
-	@After("execution(* get*(..))")
+	@Before("allGetters()")
 	public void secondAdvice() {
-		System.out.println("After Get Method.");
+		System.out.println("Second Advice run.");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters() {
+		System.out.println("Main Advice");
 	}
 
 }
